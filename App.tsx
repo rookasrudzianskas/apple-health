7e37c72e7c7494b40a82b52326f0d956bcab4df8
Rokas <rookas.rudzenskas@gmail.com>
2023-06-16 18:56:11 +0300

    ✨ feat(App.tsx): add step count, distance and flights climbed to the app view 🎨 style(App.tsx): add styles for step count, distance and flights climbed to improve readability and aesthetics

--- App.tsx
+++ App.tsx
@@ -4,8 +4,22 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{flexDirection: 'row'}}>
+        <View style={styles.valueContainer}>
+          <Text style={styles.label}>Steps</Text>
+          <Text style={styles.value}>42343</Text>
+        </View>
+
+        <View style={styles.valueContainer}>
+          <Text style={styles.label}>Distance</Text>
+          <Text style={styles.value}>1.43km</Text>
+        </View>
+      </View>
+
+      <View style={styles.valueContainer}>
+        <Text style={styles.label}>Flights Climbed</Text>
+        <Text style={styles.value}>2</Text>
+      </View>
     </View>
   );
 }
@@ -14,7 +28,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 12,
   },
+  value: {
+    fontSize: 35,
+    color: '#252525',
+  },
+  valueContainer: {
+    marginRight: 50
+  },
+  label: {
+
+  }
 });
