b4a606daaaf761d5bf96e4745dbba8b30a54dff4
Rokas <rookas.rudzenskas@gmail.com>
2023-06-16 19:23:40 +0300

    🎨 style(RingProgress.tsx): remove unnecessary viewBox attribute and add cx and cy attributes to Circle component to center it properly

--- src/components/RingProgress.tsx
+++ src/components/RingProgress.tsx
@@ -12,8 +12,10 @@ const color = "#EE0F55"
 const RingProgress = ({radius = 100}: RingProgressProps) => {
   return (
     <View style={{width: radius * 2, height: radius * 2, alignSelf: 'center'}}>
-      <Svg height="50%" width="50%" viewBox="0 0 100 100">
+      <Svg>
         <Circle
+          cx={radius}
+          cy={radius}
           r={radius}
           fill={color}
         />
