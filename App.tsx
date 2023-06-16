4ceda3b9b6d864df1b535b24059e96b643c72ee8
Rokas <rookas.rudzenskas@gmail.com>
2023-06-16 19:00:07 +0300

    🎨 style(App.tsx): extract Value component to improve code readability and reusability

--- App.tsx
+++ App.tsx
@@ -1,24 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const Value = ({label, value}: {label: string, value: string}) => {
+  return (
+    <View style={styles.valueContainer}>
+      <Text style={styles.label}>Steps</Text>
+      <Text style={styles.value}>42343</Text>
+    </View>
+  )
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={{flexDirection: 'row'}}>
-        <View style={styles.valueContainer}>
-          <Text style={styles.label}>Steps</Text>
-          <Text style={styles.value}>42343</Text>
-        </View>
-
-        <View style={styles.valueContainer}>
-          <Text style={styles.label}>Distance</Text>
-          <Text style={styles.value}>1.43km</Text>
-        </View>
-      </View>
-
-      <View style={styles.valueContainer}>
-        <Text style={styles.label}>Flights Climbed</Text>
-        <Text style={styles.value}>2</Text>
+        <Value label={'Steps'} value={'43243'} />
+        <Value label={'Steps'} value={'43243'} />
+        <Value label={'Distance'} value={'1.43km'} />
+        <Value label={'Flights Climbed'} value={'2'} />
       </View>
     </View>
   );
